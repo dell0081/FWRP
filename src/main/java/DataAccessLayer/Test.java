5fca62664e2f00d7c643b0d4643b96b3753af8a5
dell0081 <mahdydellawari38@gmail.com>
2024-07-27 14:47:09 -0400

    DATABASE

---
+++ src/main/java/DataAccessLayer/Test.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DataAccessLayer;
+
+import java.sql.Connection;
+
+/**
+ * TEST YOUR DATABASE CONNECTION IF IT WORK
+ * @author Mohammad
+ */
+public class Test {
+    
+    public static void main(String[] args){
+      
+        Connection con = DBConnection.getInstance().getConnection();
+    if(con != null){
+    System.out.print("Database connection succesful!");
+        }else{
+        System.out.print("Wrong schema name!");
+      }
+    }
+}
+   
